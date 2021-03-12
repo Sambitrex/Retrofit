823cd5afe25cc2bf74d2b3880e488de135506670
Sambitrex <sasha54364@yadex.ru>
2021-03-12 23:39:37 +0300

    Initial commit

--- app/src/main/java/android/example/retrofit/MainActivity.java
+++ app/src/main/java/android/example/retrofit/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -12,13 +14,15 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
 
-        new Thread(new Runnable() {
+       Thread parseThread =  new Thread(new Runnable() {
             @Override
             public void run() {
                 NetworkService.getInstance()
@@ -28,21 +32,30 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(Call<List<Currencies>> call, Response<List<Currencies>> response) {
                                 List<Currencies> list = response.body();
+                                Log.d("MyLog", list.get(0).getCur_Name());
+                                setAdapter(list);
 
-                                for(Currencies cur : list) {
-                                    Log.d("MyLog", cur.getCur_Name());
-                                    Log.d("MyLog", String.valueOf(cur.getCur_Scale()));
-                                    Log.d("MyLog", String.valueOf(cur.getCur_OfficialRate()));
-                                }
                             }
 
                             @Override
                             public void onFailure(Call<List<Currencies>> call, Throwable t) {
 
                             }
+
                         });
 
             }
-        }).start();
+        });
+
+        parseThread.start();
+
+
+
+    }
+
+    public void setAdapter(List<Currencies> list){
+        CurrenciesListAdapter adapter =
+                new CurrenciesListAdapter(this, R.layout.adapter_view_list, new ArrayList<Currencies>(list));
+        listView.setAdapter(adapter);
     }
 }
